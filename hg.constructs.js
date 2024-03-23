ce58d64cc7362ca8799f5d681a1407b07d8e66a6
wizard1146 <163006017+wizard1146@users.noreply.github.com>
2024-03-23 13:23:33 +1100

    Update hg.constructs.js

--- hg.constructs.js
+++ hg.constructs.js
@@ -1,5 +1,15 @@
+hg = typeof hg != 'undefined' ? hg : {}
 
 hg.constructs = (function() {
+  let ModDeckDefaults = [
+     -2,
+     -1,-1,-1,-1,-1,
+      0, 0, 0, 0, 0, 0,
+      1, 1, 1, 1, 1,
+      2,
+      'null',
+      'crit',
+  ]
 
   class Character {
     constructor() {
@@ -11,7 +21,32 @@ hg.constructs = (function() {
     }
   }
 
+  class ModDeck {
+    constructor() {
+      this.deck = []
+      ModDeckDefaults.forEach(card => {
+        this.deck.push(new ModCard(card))
+      })
+    }
+
+    static async new(arg) {
+      return new ModDeck(arg)
+    }
+  }
+
+  class ModCard {
+    constructor(mod, args) {
+      this.modifier = mod ? mod : 0
+      this.add      = false
+      this.roll     = false
+      this.bless    = false
+      this.curse    = false
+      this.shuffle  = false
+      this.origin   = args ? (args?.origin ? args?.origin : 'default') : 'default'
+    }
+  }
+
   return {
-  
+    modifierDeck: ModDeck,
   }
 })()
