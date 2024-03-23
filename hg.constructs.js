40defeb2d9919237b6825d2fc3d9a87e30b1623c
wizard1146 <163006017+wizard1146@users.noreply.github.com>
2024-03-23 16:54:09 +1100

    Update hg.constructs.js

--- hg.constructs.js
+++ hg.constructs.js
@@ -10,10 +10,27 @@ hg.constructs = (function() {
       'null',
       'crit',
   ]
+  let characterResources = {
+    gold: 30,
+    lumber: 0, metal: 0, hide: 0,
+    arrowvine: 0, flamefruit: 0, axenut: 0, rockroot: 0, corpsecap: 0, snowthistle: 0,
+  }
 
   class Character {
-    constructor() {
+    constructor(args) {
+      this.uuid    = uuid()
+      this.name    = false
+      this.class   = false
+      this.level   = 1
+      this.xp      = 0
       
+      Object.entries(characterResources).forEach(([k,v],index) => {
+        this[k]  = v
+      })
+      
+      if (args) Object.entries(args).forEach(([k,v],i) => {
+        this[k]  = v
+      })
     }
     
     static async create() {
@@ -45,8 +62,17 @@ hg.constructs = (function() {
       this.origin   = args ? (args?.origin ? args?.origin : 'default') : 'default'
     }
   }
+  
+  // Helper functions
+  /* UUID: https://stackoverflow.com/a/2117523 */
+  let uuid = function() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+  }
 
   return {
+    character   : Character,
     modifierDeck: ModDeck,
   }
 })()
